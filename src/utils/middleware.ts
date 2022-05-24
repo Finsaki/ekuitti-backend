776ac0e490c6bf021d26b5174c154eeb62600d6a
Joona Gynther <joona.gynther@cgi.com>
2022-05-24 11:46:00 +0300

    Fix typings in middlewares

--- src/utils/middleware.ts
+++ src/utils/middleware.ts
@@ -1,18 +1,15 @@
-import { Request, Response } from 'express'
+import { Request, Response, NextFunction } from 'express'
 import morgan from 'morgan'
 import * as logger from './logger'
-import jwt from 'jsonwebtoken'
+import { verify, JwtPayload } from 'jsonwebtoken'
 import { getItem } from '../models/userDao'
 
 //This file contains helper functions which are used in response to API calls
 
 //setting up a custom morgan token that shows the request body data when the operation is a POST operation
-// eslint-disable-next-line no-unused-vars
-morgan.token('response-body', function (req: Request, res: Response) {
+morgan.token('response-body', function (req: Request) {
   if (req.method === 'POST') {
-    if (req.body.password) {
-      req.body.password = '****'
-    }
+    if (req.body.password) req.body.password = '****'
     return JSON.stringify(req.body)
   }
   return null
@@ -24,23 +21,38 @@ const unknownEndpoint = (_req: Request, res: Response) => {
 }
 
 //This adds the user as a new field to a request for easy access from anywhere in the app
-const userExtractor = async (req: Request, res: Response, next: any) => {
+const userExtractor = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   //checking that token field in request matches decodedToken with envSecret value
-  const decodedToken: any = jwt.verify(req.headers.cookie, process.env.SECRET)
+  const decodedToken = verify(
+    req.headers.cookie,
+    process.env.SECRET
+  ) as JwtPayload // Tell tsc that token is a jwtpayload not a string
+
   if (!req.headers.cookie || !decodedToken.id) {
     return res.status(401).json({ error: 'token missing or invalid' })
   }
+
   const user = await getItem(decodedToken.id)
   //all id's are used for verification processes
   req.user = {
     id: user.id,
     eAddressId: user.eAddressId,
-    receiptIds: user.receiptIds
+    receiptIds: user.receiptIds,
   }
+
   next()
 }
 
-const errorHandler = (error: Error, _req: Request, res: Response, next: any) => {
+const errorHandler = (
+  error: Error,
+  _req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   //Define spesific errors here to get custom error messages
   switch (error.name) {
     case 'JsonWebTokenError': {
@@ -50,11 +62,22 @@ const errorHandler = (error: Error, _req: Request, res: Response, next: any) => 
       return res.status(401).json({ error: 'token expired' })
     }
     case 'Error': {
-      if (error.message.startsWith('Entity with the specified id already exists in the system')) {
-        return res.status(500).json({ error: 'database: values must be unique' })
-
-      } else if (error.message.startsWith('Entity with the specified id does not exist in the system')) {
-        return res.status(500).json({ error: 'database: values matching given id not found' })
+      if (
+        error.message.startsWith(
+          'Entity with the specified id already exists in the system'
+        )
+      ) {
+        return res
+          .status(500)
+          .json({ error: 'database: values must be unique' })
+      } else if (
+        error.message.startsWith(
+          'Entity with the specified id does not exist in the system'
+        )
+      ) {
+        return res
+          .status(500)
+          .json({ error: 'database: values matching given id not found' })
       }
       break
     }
@@ -67,9 +90,4 @@ const errorHandler = (error: Error, _req: Request, res: Response, next: any) => 
   next(error)
 }
 
-export {
-  morgan,
-  unknownEndpoint,
-  errorHandler,
-  userExtractor
-}
+export { morgan, unknownEndpoint, errorHandler, userExtractor }
